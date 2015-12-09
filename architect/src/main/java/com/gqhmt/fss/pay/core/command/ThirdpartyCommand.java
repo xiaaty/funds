e6528b25f7de9aa1a76ae27d42b5d33bab27a416
于泳 <yuyong@gqhmt.com>
2015-12-09 22:29:26 +0800

    '目录整理'

--- architect/src/main/java/com/gqhmt/fss/pay/core/command/ThirdpartyCommand.java
+++
@@ -1,9 +0,0 @@
-package com.gqhmt.fss.pay.core.command;
-
-/**
- * Created by yuyonf on 15/3/29.
- */
-public interface ThirdpartyCommand {
-
-//    public CommandResponse command(CommandEnum commandEnum,FundOrderEntity fundOrderEntity,Object... object);
-}
