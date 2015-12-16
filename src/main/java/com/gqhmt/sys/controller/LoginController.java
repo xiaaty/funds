0421af460a98ebb0233edefdd6999b15dfe67835
于泳 <yuyong@gqhmt.com>
2015-12-16 23:54:18 +0800

    'main'

--- src/main/java/com/gqhmt/sys/controller/LoginController.java
+++ src/main/java/com/gqhmt/sys/controller/LoginController.java
@@ -58,7 +58,7 @@ public class LoginController {
 //				sysLogWriteMapper.insertSysLog(new SysLog(request,sysUsers));
 				GlobalConstants.setSession(request, GlobalConstants.SESSION_MENU, sessionMenu);
 				if(sessionMenu.size() >= 0){
-                    return "redirect:"+getUrl(sessionMenu.get(0));
+                    return "redirect:/main";
 				}
 			}
 			model.addAttribute("errorCode","0003");
@@ -71,5 +71,11 @@ public class LoginController {
             }
             return getUrl(func.getChild().get(0));
         }
+
+	@RequestMapping("/main")
+	public String menu(){
+
+		return "main";
+	}
 		
 }
