c2207a1b075c1fc079bdb9fd244071da56ec944f
于泳 <yuyong@gqhmt.com>
2016-02-17 18:29:44 +0800

    代码整理,与富友交互代码优化,独立交互代码

--- src/main/java/com/gqhmt/pay/Service/impl/FundsTenderImpl.java
+++ src/main/java/com/gqhmt/pay/Service/impl/FundsTenderImpl.java
@@ -1,12 +1,17 @@
-package com.gqhmt.fss.logicService.pay.impl;
+package com.gqhmt.pay.service.impl;
 
 import com.gqhmt.business.architect.loan.entity.Bid;
 import com.gqhmt.business.architect.loan.entity.BidRepayment;
 import com.gqhmt.business.architect.loan.entity.Tender;
+import com.gqhmt.business.architect.loan.service.BidService;
+import com.gqhmt.business.architect.loan.service.TenderService;
 import com.gqhmt.core.FssException;
 import com.gqhmt.core.util.GlobalConstants;
-import com.gqhmt.fss.logicService.pay.IFundsTender;
-import com.gqhmt.fss.logicService.pay.PaySuperByFuiou;
+import com.gqhmt.funds.architect.account.service.FundAccountService;
+import com.gqhmt.funds.architect.order.service.FundOrderService;
+import com.gqhmt.pay.exception.CommandParmException;
+import com.gqhmt.pay.service.IFundsTender;
+import com.gqhmt.pay.service.PaySuperByFuiou;
 import com.gqhmt.funds.architect.account.entity.FundAccountEntity;
 import com.gqhmt.funds.architect.order.entity.FundOrderEntity;
 
@@ -15,7 +20,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 /**
- * Filename:    com.gqhmt.fss.logicService.pay.impl.FundsTenderImpl
+ * Filename:    com.gqhmt.pay.service.impl.FundsTenderImpl
  * Copyright:   Copyright (c)2015
  * Company:     冠群驰骋投资管理(北京)有限公司
  *
@@ -30,39 +35,51 @@ import java.util.List;
  * -----------------------------------------------------------------
  * 15/12/29  于泳      1.0     1.0 Version
  */
-public class FundsTenderImpl extends AccountAbstractCommand implements IFundsTender {
+public class FundsTenderImpl  implements IFundsTender {
 
     @Resource
     private PaySuperByFuiou paySuperByFuiou;
 
+    @Resource
+    private TenderService tenderService;
+
+    @Resource
+    private FundAccountService fundAccountService;
+
+    @Resource
+    private BidService bidService;
+
+    @Resource
+    private FundOrderService fundOrderService;
+
+
     @Override
     public boolean bid(String thirdPartyType, Long tenderId) throws FssException {
-        Tender tender = super.tenderService.findById(tenderId);
-        FundAccountEntity fromEntity = super.getFundAccount(tender.getCustomerId(), tender.getInvestType() == 1 ? 3 : 2);
-        super.hasEnoughBanlance(fromEntity,tender.getRealAmount());
+        Tender tender = this.tenderService.findById(tenderId);
+        FundAccountEntity fromEntity = this.getFundAccount(tender.getCustomerId(), tender.getInvestType() == 1 ? 3 : 2);
+        this.hasEnoughBanlance(fromEntity,tender.getRealAmount());
 
-        Bid bid = super.bidService.findById(tender.getBidId());
+        Bid bid = this.bidService.findById(tender.getBidId());
         int cusId = bid.getCustomerId();
         if (bid.getIsHypothecarius() != null && bid.getIsHypothecarius() == 1 && bid.getHypothecarius() > 0) {
             cusId = bid.getHypothecarius();
         }
 
         // 入账账户
-        FundAccountEntity toSFEntity = super.getFundAccount(cusId, GlobalConstants.ACCOUNT_TYPE_LOAN);
+        FundAccountEntity toSFEntity = this.getFundAccount(cusId, GlobalConstants.ACCOUNT_TYPE_LOAN);
         // 冻结账户
-        FundAccountEntity toEntity = super.getFundAccount(tender.getCustomerId(), GlobalConstants.ACCOUNT_TYPE_FREEZE);
+        FundAccountEntity toEntity = this.getFundAccount(tender.getCustomerId(), GlobalConstants.ACCOUNT_TYPE_FREEZE);
         BigDecimal amount = tender.getRealAmount();
         BigDecimal boundsAmount = tender.getBonusAmount();
         paySuperByFuiou.preAuth(fromEntity,toSFEntity,amount,GlobalConstants.ORDER_BID,tenderId,GlobalConstants.BUSINESS_BID);
-
         //后续处理
         return true;
     }
 
     @Override
     public boolean sellte(String thirdPartyType, long bidId) throws FssException {
-        Bid bid = super.bidService.findById(bidId);
-        List<FundOrderEntity> listFundOrder = super.fundOrderService.queryFundOrder(GlobalConstants.ORDER_SETTLE, GlobalConstants.BUSINESS_SETTLE, bid.getId());
+        Bid bid = this.bidService.findById(bidId);
+        List<FundOrderEntity> listFundOrder = this.fundOrderService.queryFundOrder(GlobalConstants.ORDER_SETTLE, GlobalConstants.BUSINESS_SETTLE, bid.getId());
 
         checkRepat(listFundOrder);
 
@@ -73,7 +90,7 @@ public class FundsTenderImpl extends AccountAbstractCommand implements IFundsTen
         //产品名称，如果产品名称为空，则去标的title
 //        String title = getProductName(bid);
 
-        FundAccountEntity toEntity = super.getFundAccount(cusId, GlobalConstants.ACCOUNT_TYPE_LOAN);// .getFundAccount(cusId,
+        FundAccountEntity toEntity = this.getFundAccount(cusId, GlobalConstants.ACCOUNT_TYPE_LOAN);// .getFundAccount(cusId,
         List<Tender> list = null;//tenderService.queryTenderByBidId(bid.getId());
         FundOrderEntity fundOrderEntity = paySuperByFuiou.createOrder(toEntity, bid.getBidAmount(), GlobalConstants.ORDER_SETTLE, bid.getId(), GlobalConstants.BUSINESS_SETTLE, thirdPartyType);
 //        Map<Integer, String> map = fuiouPreauthService.getContractNo(bid.getId().longValue());
@@ -105,9 +122,9 @@ public class FundsTenderImpl extends AccountAbstractCommand implements IFundsTen
 
     @Override
     public boolean repayment(String thirdPartyType, long bidId, long bidRepaymentId) throws FssException {
-        Bid bid = super.bidService.findById(bidId);
+        Bid bid = this.bidService.findById(bidId);
         BidRepayment bidRepayment = null;
-        List<FundOrderEntity> listFundOrder = super.fundOrderService.queryFundOrder(GlobalConstants.ORDER_REPAYMENT, GlobalConstants.BUSINESS_REPAYMENT, bidRepayment.getId());
+        List<FundOrderEntity> listFundOrder = this.fundOrderService.queryFundOrder(GlobalConstants.ORDER_REPAYMENT, GlobalConstants.BUSINESS_REPAYMENT, bidRepayment.getId());
         checkRepat(listFundOrder);
 
         Integer cusId = bid.getCustomerId();
@@ -151,7 +168,7 @@ public class FundsTenderImpl extends AccountAbstractCommand implements IFundsTen
 
     @Override
     public boolean abortByBid(String thirdPartyType, long bidId) throws FssException {
-        Bid bid = super.bidService.findById(bidId);
+        Bid bid = this.bidService.findById(bidId);
 //        List<Tender> list = tenderService.queryTenderByBidId(bid.getId());
 //        FundAccountEntity toSFEntity = super.getFundAccount(bid.getCustomerId(), GlobalConstants.ACCOUNT_TYPE_LOAN);
 //        Map<Integer, String> map = fuiouPreauthService.getContractNo(bid.getId().longValue());
@@ -171,6 +188,27 @@ public class FundsTenderImpl extends AccountAbstractCommand implements IFundsTen
             } else {
                 throw new FssException("请勿重复提交");
             }
+        }
+    }
+
+    private FundAccountEntity getFundAccount(int cusID, int type) throws CommandParmException {
+        FundAccountEntity entity = null;
+        if (cusID < 100) {
+            entity = fundAccountService.getFundAccount(cusID, GlobalConstants.ACCOUNT_TYPE_PRIMARY);
+        } else {
+            entity = fundAccountService.getFundAccount(cusID, type);
+        }
+
+        if (entity == null) {
+            throw new CommandParmException("账户不存在");
+        }
+        return entity;
+    }
+
+    private void hasEnoughBanlance(FundAccountEntity entity, BigDecimal amount) throws CommandParmException {
+        BigDecimal bigDecimal = entity.getAmount();
+        if (bigDecimal.compareTo(amount) < 0) {
+            throw new CommandParmException("账户余额不足");
         }
     }
 }
