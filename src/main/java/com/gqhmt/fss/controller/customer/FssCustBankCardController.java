e5a748930470dcd3bb099d1724419ae66120801a
柯禹来 <57627@DESKTOP-2EL7R4M>
2016-02-19 09:32:47 +0800

    删除重复方法名

--- src/main/java/com/gqhmt/fss/controller/customer/FssCustBankCardController.java
+++ src/main/java/com/gqhmt/fss/controller/customer/FssCustBankCardController.java
@@ -54,29 +54,6 @@ public class FssCustBankCardController {
 		model.addAttribute("customer", customer);
 		return "fss/customer/bankCardList";
 	}
-	
-	/**
-	 * author:kyl 
-	 * time:2016年2月16日 
-	 * author:jhz 
-	 * time:2016年1月25日 
-	 * function：银行卡信息列表
-	 */
-	@RequestMapping(value = "/fss/customer/bankCards", method = {RequestMethod.GET,RequestMethod.POST})
-	@AutoPage
-	public Object bankCardList(HttpServletRequest request, ModelMap model,
-			@ModelAttribute(value = "customer") CustomerAndUser customerAndUser) {
-		// 得到银行卡、客户信息列表
-		List<CustomerAndUser> bankCards = bankCardService.findbankCardAll(customerAndUser);
-		//得到银行列表
-		List<BankCardBean> banks=bankCardInfoService.queryBankList();
-		
-		model.addAttribute("page", bankCards);
-		model.addAttribute("banks", banks);
-		// model.addAttribute("customer", customerAndUser);
-		return "fss/customer/bankCardList";
-	}
-
 	/**
 	 * author:jhz 
 	 * time:2016年1月25日 
