50b1c4bfe05b150b648383f577ea8d221330b7ca
于泳 <yuyong@gqhmt.com>
2016-03-01 16:13:05 +0800

    添加验证,bug修复

--- src/main/java/com/gqhmt/pay/service/TradeRecordService.java
+++ src/main/java/com/gqhmt/pay/service/TradeRecordService.java
@@ -67,8 +67,8 @@ public class TradeRecordService {
 //        createFundTrade(fromEntity, BigDecimal.ZERO, amount, 3001, "出借" + title + "，冻结账户资金 " + amount + "元" + (boundsAmount !=null ? ",红包抵扣资金 " + boundsAmount + "元" : ""), (boundsAmount != null? boundsAmount : BigDecimal.ZERO));
     }
 
-    public void transfer(FundAccountEntity fromAcc,FundAccountEntity toAcc,BigDecimal amount,int fundType,int  accountType,FundOrderEntity fundOrderEntity) throws FssException {
-        sequenceService.transfer(fromAcc,toAcc,amount,fundType,accountType,null,ThirdPartyType.FUIOU,fundOrderEntity);
+    public void transfer(FundAccountEntity fromAcc,FundAccountEntity toAcc,BigDecimal amount,int  fundType,FundOrderEntity fundOrderEntity) throws FssException {
+        sequenceService.transfer(fromAcc,toAcc,amount,8,fundType,null,ThirdPartyType.FUIOU,fundOrderEntity);
     }
     /**
      * 交易记录查询
