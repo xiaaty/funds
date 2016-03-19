687ff57f847b2a9f12253ebcfc77aba01ece9184
于泳 <yuyong@gqhmt.com>
2016-03-19 17:59:34 +0800

    连接外部服务获取数据

---
+++ src/main/java/com/gqhmt/extServInter/callbackService/PullDataService.java
@@ -0,0 +1,22 @@
+package com.gqhmt.extServInter.callbackService;
+
+/**
+ * Filename:    com.gqhmt.extServInter.callbackService.PullDataService
+ * Copyright:   Copyright (c)2015
+ * Company:     冠群驰骋投资管理(北京)有限公司
+ *
+ * @author 于泳
+ * @version: 1.0
+ * @since: JDK 1.7
+ * Create at:   16/3/19 16:41
+ * Description:
+ * <p/>
+ * Modification History:
+ * Date    Author      Version     Description
+ * -----------------------------------------------------------------
+ * 16/3/19  于泳      1.0     1.0 Version
+ */
+public class PullDataService {
+
+
+}
