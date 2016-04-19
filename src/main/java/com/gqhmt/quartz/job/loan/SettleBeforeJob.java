70248e8ecb11301d93273895b3f7a113ab27db45
jiahezhen <jiahezhen@qq.com>
2016-04-19 16:30:53 +0800

    打开定时任务

--- src/main/java/com/gqhmt/quartz/job/loan/SettleBeforeJob.java
+++ src/main/java/com/gqhmt/quartz/job/loan/SettleBeforeJob.java
@@ -37,7 +37,7 @@ public class SettleBeforeJob extends SupperJob {
     @Resource
     private BidSettleService settleService;
 
-//    @Scheduled(cron="0 0/1 *  * * * ")
+    @Scheduled(cron="0 0/1 *  * * * ")
     public void execute() throws PayChannelNotSupports {
 
         System.out.println("借款业务满满标 执行满标转账 ftp 上传记录 跑批");
