cc7c03d353554fc362cf479d76346cc64b963ba1
柯禹来 <1348750586@qq.com>
2016-04-22 19:23:43 +0800

    提现bug修改

--- src/main/java/com/gqhmt/funds/architect/order/mapper/read/FundOrderReadMapper.java
+++ src/main/java/com/gqhmt/funds/architect/order/mapper/read/FundOrderReadMapper.java
@@ -32,7 +32,7 @@ public interface FundOrderReadMapper extends ReadMapper<FundOrderEntity> {
 
     public List<FundOrderEntity> queryFundOrder(Map<Object,Object> map);
 
-    public int getWithdrawNum(@Param("accountID") Long accountID);
+    public int getWithdrawNum(@Param("accountId") Long accountId);
 
     public List<FundOrderBean> getFundOrderRechargeAndWithdraw(@Param("custId") Integer custId,@Param("order_type")int orderType,@Param("strTime")String strTime,@Param("endTime")String endTime);
     
