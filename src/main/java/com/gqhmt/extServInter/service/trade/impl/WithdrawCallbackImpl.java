2f718167365432ba3c2d5204203fd92c889afadf
柯禹来 <1348750586@qq.com>
2016-04-26 11:37:34 +0800

    添加了充值提现成功发送站内短信

--- src/main/java/com/gqhmt/extServInter/service/trade/impl/WithdrawCallbackImpl.java
+++ src/main/java/com/gqhmt/extServInter/service/trade/impl/WithdrawCallbackImpl.java
@@ -1,6 +1,5 @@
 package com.gqhmt.extServInter.service.trade.impl;
 
-import com.gqhmt.annotations.APISignature;
 import com.gqhmt.annotations.APITradeTypeValid;
 import com.gqhmt.core.FssException;
 import com.gqhmt.core.util.LogUtil;
