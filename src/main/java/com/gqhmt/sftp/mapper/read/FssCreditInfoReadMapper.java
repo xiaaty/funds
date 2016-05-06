6d37a3713d8b95a6fc40d5b662d96f2a7c665e61
jiahezhen <jiahezhen@qq.com>
2016-05-06 18:32:37 +0800

    项目信息

---
+++ src/main/java/com/gqhmt/sftp/mapper/read/FssCreditInfoReadMapper.java
@@ -0,0 +1,24 @@
+package com.gqhmt.sftp.mapper.read;
+
+import com.gqhmt.core.mybatis.ReadMapper;
+import com.gqhmt.sftp.entity.FssCreditInfoEntity;
+
+/**
+ * 
+ * Filename:    com.gqhmt.extServInter.dto.account.CreateAccountByFuiou
+ * Copyright:   Copyright (c)2016
+ * Company:     冠群驰骋投资管理(北京)有限公司
+ *
+ * @author jhz
+ * @version: 1.0
+ * @since: JDK 1.7
+ * Create at:   2016年5月6日
+ * Description:
+ * <p>
+ * Modification History:
+ * Date    Author      Version     Description
+ * -----------------------------------------------------------------
+ * 2016年5月6日  jhz      1.0     1.0 Version
+ */
+public interface FssCreditInfoReadMapper extends ReadMapper<FssCreditInfoEntity> {
+}
