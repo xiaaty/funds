88fc9c48cc276db1b8c6ac4195969e45ff22f992
于泳 <yuyong@gqhmt.com>
2016-05-20 15:11:33 +0800

    出借你服务费退回

---
+++ src/main/java/com/gqhmt/core/redis/RedisManager.java
@@ -0,0 +1,20 @@
+package com.gqhmt.core.redis;
+
+/**
+ * Filename:    com.gqhmt.core.redis.RedisManager
+ * Copyright:   Copyright (c)2015
+ * Company:     冠群驰骋投资管理(北京)有限公司
+ *
+ * @author 于泳
+ * @version: 1.0
+ * @since: JDK 1.7
+ * Create at:   16/5/19 10:26
+ * Description:
+ * <p/>
+ * Modification History:
+ * Date    Author      Version     Description
+ * -----------------------------------------------------------------
+ * 16/5/19  于泳      1.0     1.0 Version
+ */
+public class RedisManager {
+}
