0e6e9046622fb402e451988b27c85e6f2d2e6406
柯禹来 <1348750586@qq.com>
2016-06-14 12:39:22 +0800

    错误修改代码提交

--- funds-web/src/main/java/com/gqhmt/fss/architect/loan/service/FssLoanService.java
+++ funds-web/src/main/java/com/gqhmt/fss/architect/loan/service/FssLoanService.java
@@ -1,6 +1,6 @@
 package com.gqhmt.fss.architect.loan.service;
 
-import com.gqhmt.core.FssException;
+import com.gqhmt.core.exception.FssException;
 import com.gqhmt.core.util.Application;
 import com.gqhmt.core.util.GlobalConstants;
 import com.gqhmt.extServInter.dto.loan.*;
