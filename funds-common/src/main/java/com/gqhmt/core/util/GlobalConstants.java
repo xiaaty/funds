47c9e57edb591eec3436fdd98d59b58f3ee12687
于泳 <yuyong@gqhmt.com>
2016-07-04 13:52:32 +0800

    修改富友回盘地址过滤设置

--- funds-common/src/main/java/com/gqhmt/core/util/GlobalConstants.java
+++ funds-common/src/main/java/com/gqhmt/core/util/GlobalConstants.java
@@ -18,7 +18,7 @@ public class GlobalConstants {
 	public static final String SESSION_EMP = "emp_session";
 	public static final String SESSION_MENU = "session_menu";
 	public static final String EXCLUDE_URL_INIT="exclude";
-	public static final String EXCLUDE_DIRECTORY_REGEX="^/css/|^/images/|^/js/|^/upload/|^/img/|^/api/|^/fonts/|^/loginout.jsp";
+	public static final String EXCLUDE_DIRECTORY_REGEX="^/css/|^/images/|^/js/|^/upload/|^/img/|^/api/|^/interaction/|^/fonts/|^/loginout.jsp";
 	public static final String ROLE_SUPPER_ID=",1,";
     public static final String ROLE_BUSINESS_SUPPER_ID=",2,";
 	public static final String EXCLUDE_ONLY_LOGIN="^/json/";
