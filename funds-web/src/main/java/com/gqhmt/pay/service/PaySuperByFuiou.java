4162da07b39697c57178b5a64e71547f4960d094
柯禹来 <1348750586@qq.com>
2016-07-22 18:12:16 +0800

    转账是报类型转换异常bug

--- funds-web/src/main/java/com/gqhmt/pay/service/PaySuperByFuiou.java
+++ funds-web/src/main/java/com/gqhmt/pay/service/PaySuperByFuiou.java
@@ -168,9 +168,9 @@ public class PaySuperByFuiou {
         FundOrderEntity fundOrderEntity = this.createOrder(fromEntity,toEntity,amount,orderType,busiId,busiType,newOrderType,tradeType,lendNo,toLendNo,loanCustId,loanNo);
         CommandResponse response = null;
         if(fromEntity.getCustId() < 100 || toEntity.getCustId() < 100){
-            response = ThirdpartyFactory.command(thirdPartyType, PayCommondConstants.COMMAND_CHARGE_WITHDRAW, fundOrderEntity, fromEntity.getUserName(),toEntity.getUserName(),amount,contractNo);
+            response = ThirdpartyFactory.command(thirdPartyType, PayCommondConstants.COMMAND_CHARGE_WITHDRAW, fundOrderEntity, fromEntity.getUserName(),toEntity.getUserName(),amount);
         }else {
-            response = ThirdpartyFactory.command(thirdPartyType, PayCommondConstants.COMMAND_TRADE_TRANSFER, fundOrderEntity, fromEntity, toEntity, amount);
+            response = ThirdpartyFactory.command(thirdPartyType, PayCommondConstants.COMMAND_TRADE_TRANSFER, fundOrderEntity, fromEntity, toEntity, amount,contractNo);
         }
         response.setFundOrderEntity(fundOrderEntity);
         return response;
