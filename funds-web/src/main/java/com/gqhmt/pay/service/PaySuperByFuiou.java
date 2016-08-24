a58d31cdac3ea3f6eaeae61965bdd787fde41d64
于泳 <yuyong@gqhmt.com>
2016-08-24 11:13:44 +0800

    增加日志打印功能AOP

--- funds-web/src/main/java/com/gqhmt/pay/service/PaySuperByFuiou.java
+++ funds-web/src/main/java/com/gqhmt/pay/service/PaySuperByFuiou.java
@@ -105,7 +105,7 @@ public class PaySuperByFuiou {
      */
     public boolean changeCard(FundAccountEntity primaryAccount,String cardNo, String bankCd, String bankNm,
                                 String cityId, String fileName,String tradeType) throws FssException {
-        LogUtil.info(this.getClass(),"第三方个人提现规则设置:"+primaryAccount.getAccountNo()+":"+cardNo+":"+bankCd+":"+bankNm+":"+cityId+":"+fileName);
+        LogUtil.info(this.getClass(),"银行卡变更:"+primaryAccount.getAccountNo()+":"+cardNo+":"+bankCd+":"+bankNm+":"+cityId+":"+fileName);
         FundOrderEntity fundOrderEntity = this.createOrder(primaryAccount,BigDecimal.ZERO,GlobalConstants.ORDER_UPDATE_CARD,0,0,"",tradeType);
         CommandResponse response =ThirdpartyFactory.command(thirdPartyType, PayCommondConstants.COMMAND_ACCOUNT_FUIOU_CARD, fundOrderEntity, primaryAccount,cardNo,bankNm,bankCd,cityId,fileName);
         return execExction(response,fundOrderEntity);
