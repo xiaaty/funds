ed766c3536d5247d6424e8e0b9f184925d4fc09b
柯禹来 <1348750586@qq.com>
2016-09-06 16:59:23 +0800

    冠E贷转账充值、费用接口（线下垫付利息）修改

--- funds-web/src/main/java/com/gqhmt/pay/service/trade/impl/FundsTradeImpl.java
+++ funds-web/src/main/java/com/gqhmt/pay/service/trade/impl/FundsTradeImpl.java
@@ -684,7 +684,7 @@ public class FundsTradeImpl  implements IFundsTrade {
             throw new CommandParmException("90004009");
         }
        //创建充值记录信息
-        FssOfflineRechargeEntity  fssOfflineRechargeEntity=fssOfflineRechargeService.createOfflineRecharge("1103", primaryAccount.getCustId(), primaryAccount.getCustName(),cust_type,amt,trade_type,seq_no,mchn);
+        FssOfflineRechargeEntity  fssOfflineRechargeEntity=fssOfflineRechargeService.createOfflineRecharge("1103", primaryAccount.getCustId(), primaryAccount.getCustName(),cust_type,amt,trade_type,seq_no,mchn,busi_no);
         CommandResponse response = paySuperByFuiou.offlineRecharge(primaryAccount,amt,GlobalConstants.ORDER_CHARGE,fssOfflineRechargeEntity.getId(),0);
         //根据返回码判断是否成功，修改线下充值记录状态
         if("0000".equals(response.getCode())){//成功
