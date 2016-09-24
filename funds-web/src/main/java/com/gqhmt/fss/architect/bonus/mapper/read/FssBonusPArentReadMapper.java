0c18dce4529fb79c5e15eea60b4ecde9e905a985
jiahezhen <jiahezhen@qq.com>
2016-09-24 21:32:24 +0800

    New changelist

--- funds-web/src/main/java/com/gqhmt/fss/architect/bonus/mapper/read/FssBonusPArentReadMapper.java
+++ funds-web/src/main/java/com/gqhmt/fss/architect/bonus/mapper/read/FssBonusPArentReadMapper.java
@@ -1,10 +1,11 @@
 package com.gqhmt.fss.architect.bonus.mapper.read;
 
 import com.gqhmt.core.mybatis.ReadMapper;
-import com.gqhmt.fss.architect.bonus.entity.FssBonusParentEntity;
+import com.gqhmt.fss.architect.bonus.entity.FssBonusEntity;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Filename:    com.gqhmt.fss.architect.trade.mapper.read.FssTradeRecordWriteMapper
@@ -21,14 +22,6 @@ import java.util.List;
  * -----------------------------------------------------------------
  * 2016/6/29  jhz     1.0     1.0 Version
  */
-public interface FssBonusParentReadMapper extends ReadMapper<FssBonusParentEntity>{
-
-   /**
-    * jhz
-    * @param mchn
-    * @param seqNo
-    * @return
-     */
-   public FssBonusParentEntity queryBonusParent(@Param("mchn") String mchn,@Param("seqNo") String seqNo);
+public interface FssBonusPArentReadMapper extends ReadMapper<FssBonusEntity>{
 
 }
