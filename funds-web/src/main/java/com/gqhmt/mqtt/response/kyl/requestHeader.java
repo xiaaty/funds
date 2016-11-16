0387875dcc3339d087a011116707fc8b16382adb
keyulai <keyulai@gqhmt.com>
2016-11-16 13:41:14 +0800

    删除类

---
+++ funds-web/src/main/java/com/gqhmt/mqtt/response/kyl/requestHeader.java
@@ -0,0 +1,31 @@
+package com.gqhmt.mqtt.response.kyl;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name="trans_head")
+@XmlType(name = "", propOrder = {})
+public class requestHeader {
+	
+	private String return_code;
+	private String return_msg;
+
+	public String getReturn_code() {
+		return return_code;
+	}
+
+	public void setReturn_code(String return_code) {
+		this.return_code = return_code;
+	}
+
+	public String getReturn_msg() {
+		return return_msg;
+	}
+
+	public void setReturn_msg(String return_msg) {
+		this.return_msg = return_msg;
+	}
+}
