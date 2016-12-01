2dde47dc24e6ef5edb98f4dd29475dacda57098d
keyulai <keyulai@gqhmt.com>
2016-12-01 17:22:37 +0800

    标的开户修改，冻结转账调整

--- funds-web/src/main/java/com/gqhmt/pay/service/TradeRecordService.java
+++ funds-web/src/main/java/com/gqhmt/pay/service/TradeRecordService.java
@@ -173,8 +173,7 @@ public class TradeRecordService {
             sequenceService.transfer(fromAcc, toAcc, amount, actionType, fundType, null, ThirdPartyType.FUIOU, fundOrderEntity);
         }
         //            -----------------------调用统一支付进行记账----------------
-        tyzfTradeService.tyzfTransfer(fromAcc.getCustId(),fromAcc.getBusiType(),toAcc.getCustId(),toAcc.getBusiType(),amount,"1106",fundOrderEntity.getOrderNo());
-
+        tyzfTradeService.tyzfTransfer(fromAcc.getCustId(),fromAcc.getBusiType(),toAcc.getCustId(),toAcc.getBusiType(),amount,"1106",fundOrderEntity.getOrderNo(),transf_flag);
     }
 
     /**
@@ -203,7 +202,7 @@ public class TradeRecordService {
         }else{
             sequenceService.transfer(fromAcc, toAcc, actionType, fundType, amount, memo, fundOrderEntity, newFundsType, tradeType, lendNo, toCustId, toLendNo, loanCustId, loanNo);
         }
-        tyzfTradeService.tyzfTransfer(fromAcc.getCustId(),fromAcc.getBusiType(),toAcc.getCustId(),toAcc.getBusiType(),amount,tradeType,fundOrderEntity.getOrderNo());
+        tyzfTradeService.tyzfTransfer(fromAcc.getCustId(),fromAcc.getBusiType(),toAcc.getCustId(),toAcc.getBusiType(),amount,tradeType,fundOrderEntity.getOrderNo(),transf_flag);
     }
 
     /**
