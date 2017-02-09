d69a8149302605b076230bb442c36c99b8ef4906
于泳 <yuyong@gqhmt.com>
2017-02-09 18:39:06 +0800

    新建公共组件

---
+++ common/common-frame/src/main/java/com/gqhmt/common/context/IApplicationContext.java
@@ -0,0 +1,20 @@
+package com.gqhmt.common.context;
+
+/**
+ * Filename:    com.gqhmt.common.context.IApplicationContext
+ * Copyright:   Copyright (c)2015
+ * Company:     冠群驰骋投资管理(北京)有限公司
+ *
+ * @author 于泳
+ * @version: 1.0
+ * @since: JDK 1.7
+ * Create at:   2017/2/8 17:54
+ * Description:
+ * <p/>
+ * Modification History:
+ * Date    Author      Version     Description
+ * -----------------------------------------------------------------
+ * 2017/2/8  于泳      1.0     1.0 Version
+ */
+public class IApplicationContext {
+}
